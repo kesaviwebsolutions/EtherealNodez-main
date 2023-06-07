023987a6f8edd4e02cf93cecd23bf8fa31f4110f
SAURABH YADAV <rituknj@gmail.com>
2023-06-07 11:16:28 +0530

    database

--- src/component/pages/Dashboardfiles/Datetabs1.js
+++ src/component/pages/Dashboardfiles/Datetabs1.js
@@ -10,37 +10,35 @@ export default function Datetabs() {
     {
       key: "1",
       label: `1D`,
-     
     },
     {
       key: "2",
       label: `1W`,
-    
     },
     {
       key: "3",
       label: `1M`,
-    
     },
     {
       key: "4",
       label: `1Y`,
-      
     },
   ];
   return (
     <div>
       {" "}
       <div>
-      <div className="d-f a-i-c f-f-int m-b-2 c-p">
-        <div className="m-l-0_5 f-f-int">1D</div>
-        
-        <div className="m-l-0_5 f-f-int">1W</div>
-        <div className="high_light m-l-0_5 f-f-int">1M</div>
-        <div className="m-l-0_5 f-f-int">1Y</div>
-        <div className="m-l-0_5 f-f-int"><AiOutlineCalendar className="f-s-1_5 b-c-t" /></div>
+        <div className="d-f a-i-c f-f-int m-b-2 c-p">
+          <div className="m-l-0_5 f-f-int">1D</div>
+
+          <div className="m-l-0_5 f-f-int">1W</div>
+          <div className="high_light m-l-0_5 f-f-int">1M</div>
+          <div className="m-l-0_5 f-f-int">1Y</div>
+          <div className="m-l-0_5 f-f-int">
+            <AiOutlineCalendar className="f-s-1_5 b-c-t" />
+          </div>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
